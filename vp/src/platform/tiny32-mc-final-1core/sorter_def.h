daf940cf468f8f0633a662941e54c91a5182377e
YU-LIN-LIN <61815140+YU-LIN-LIN@users.noreply.github.com>
2022-06-05 23:19:51 +0800

    Add files via upload

---
+++ vp/src/platform/tiny32-mc-final-1core/sorter_def.h
@@ -0,0 +1,20 @@
+#ifndef SORTER_DEF_H_
+#define SORTER_DEF_H_
+
+#define SEQ_LEN 9
+#define SEQ_TOT 9
+#define MAX 255
+
+const int SORTER_R_ADDR = 0x00000000;
+const int SORTER_RESULT_ADDR = 0x00000004;
+// const int SORTER_CHECK_ADDR = 0x73000008;
+
+union word {
+  int sint;
+  unsigned int uint;
+  unsigned char uc[4];
+  unsigned char ans[4];
+};
+
+
+#endif
